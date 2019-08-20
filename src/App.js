7738f1a6b00fa0721767fba2b7b0b13f3ff9b720
Joshua Smith <ws.joshua@gmail.com>
2019-08-20 11:41:04 -0600

    swapping to branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
+import React, { Component } from 'react';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './Login';
+import TaskContainer from './Components/TaskContainer/index'
+import Header from './Components/Header';
 
-function App() {
+const My404 = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      You are lost
     </div>
-  );
+  )
+}
+//Login Function passing props to Login Component inside header 
+//Register Function passing props to Register Component inside header 
+//Logout Function writing to database - 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      loginTime: null,
+      logoutTime: null,
+      loading: true,
+      tasks: [],
+      currentTime: new Date().toLocaleString()
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <TaskContainer />
+      </div>
+    );
+  }
+}
 }
 
 export default App;
+
+// not using switch until all functionality is in place -- then drop into routes. 
---
+++ src/Components/TaskContainer/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class TaskContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (<h1>This is the Task Container</h1>);
+    }
+}
+
+export default TaskContainer;
