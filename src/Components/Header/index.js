359cfd1d3cbe7be067733cbf6ed10b82af414695
Derek Hutson <denverd1301@gmail.com>
2019-08-26 08:53:22 -0600

    fixed syntax errors

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,26 +2,28 @@ import React, { Component } from "react";
 class Header extends Component {
   constructor(props) {
     super(props);
-  const headerStyle = {
- 
-  handleAddTaskClick = () => {
-    this.props.handleAddTaskClick()
   }
+
+  handleAddTaskClick = () => {
+    this.props.handleAddTaskClick();
+  };
+
   render() {
-    return (<div className="ui inverted segment">
-      <div className="ui inverted secondary menu">
-        <a className="active item" href="#">
-          Home
-      </a>
-        <a className="item" href="#" onClick={this.handleAddTaskClick}>
-          Add Task
-      </a>
-        <a className="item" href="/user/logout">
-          Logout
-      </a>
+    return (
+      <div className="ui inverted segment">
+        <div className="ui inverted secondary menu">
+          <a className="active item" href="#">
+            Home
+          </a>
+          <a className="item" href="#" onClick={this.handleAddTaskClick}>
+            Add Task
+          </a>
+          <a className="item" href="/user/logout">
+            Logout
+          </a>
+        </div>
       </div>
-    </div>);
+    );
   }
 }
 export default Header;
-
