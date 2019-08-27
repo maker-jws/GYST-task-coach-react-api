21f59dc86e77dfeb3396f73c3691597a26f04b9c
Derek Hutson <denverd1301@gmail.com>
2019-08-27 08:16:31 -0600

    user profile linked with their tasks

--- src/Components/TaskList/index.js
+++ src/Components/TaskList/index.js
@@ -1,131 +1,169 @@
-import React, { Component } from 'react';
-import { setPriority } from 'os';
+import React, { Component } from "react";
+import { setPriority } from "os";
 // import { List, Segment, Button } from 'semantic-ui-react'
 
 class TaskList extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
-  setPriorityStyle = (task) => {
+  setPriorityStyle = task => {
     const highPriorityStyle = {
       border: "red solid 2px"
-    }
+    };
     const medPriorityStyle = {
       border: "goldenrod solid 2px"
-    }
+    };
     const lowPriorityStyle = {
       border: "lightgreen solid 2px"
-    }
+    };
     const zeroPriorityStyle = {
       border: "lightblue solid 1px"
-    }
+    };
     if (task.priority === "high") {
-      return highPriorityStyle
+      return highPriorityStyle;
     } else if (task.priority === "medium") {
-      return medPriorityStyle
+      return medPriorityStyle;
     } else if (task.priority === "low") {
-      return lowPriorityStyle
+      return lowPriorityStyle;
     } else if (task.priority !== null) {
-      return zeroPriorityStyle
+      return zeroPriorityStyle;
     }
-  }
+  };
 
   render() {
-    let taskCount = 0
-    const displayLowPriorityTasks = this.props.taskList.map((task) => {
-      if (task.priority === "low") {
-        taskCount++
+    let taskCount = 0;
+    const displayLowPriorityTasks = this.props.taskList.map(task => {
+      if (
+        task.priority === "low" &&
+        this.props.currentUser.user_id === task.user_id
+      ) {
+        taskCount++;
         return (
-          <div className="ui inverted segment" key={taskCount} style={this.setPriorityStyle(task)}>
+          <div
+            className="ui inverted segment"
+            key={taskCount}
+            style={this.setPriorityStyle(task)}
+          >
             <div role="list" className="ui divided inverted relaxed list">
               <div role="listitem" className="item">
                 <div className="content">
                   <div className="header">{task.taskname}</div>
                   Priority: {task.priority}
+                  <div>Description: {task.body}</div> <br />
                   <div>
-                    Description: {task.body}
-                  </div> <br />
-                  <div>
-                    <p><button className="mini ui red button" onClick={this.props.deleteTask.bind(null, task.id)}>Del</button>
-                      <button className="mini ui button" onClick={this.props.displayEditModal.bind(null, task)}>Edit</button></p>
+                    <p>
+                      <button
+                        className="mini ui red button"
+                        onClick={this.props.deleteTask.bind(null, task.id)}
+                      >
+                        Del
+                      </button>
+                      <button
+                        className="mini ui button"
+                        onClick={this.props.displayEditModal.bind(null, task)}
+                      >
+                        Edit
+                      </button>
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        )
+        );
       }
-    })
-    const displayMediumPriorityTasks = this.props.taskList.map((task) => {
-      taskCount++
-      if (task.priority === "medium") {
+    });
+    const displayMediumPriorityTasks = this.props.taskList.map(task => {
+      taskCount++;
+      if (
+        task.priority === "medium" &&
+        this.props.currentUser.user_id === task.user_id
+      ) {
         return (
-          <div className="ui inverted segment" key={taskCount} style={this.setPriorityStyle(task)}>
+          <div
+            className="ui inverted segment"
+            key={taskCount}
+            style={this.setPriorityStyle(task)}
+          >
             <div role="list" className="ui divided inverted relaxed list">
               <div role="listitem" className="item">
                 <div className="content">
                   <div className="header">{task.taskname}</div>
                   Priority: {task.priority}
+                  <div>Description: {task.body}</div> <br />
                   <div>
-                    Description: {task.body}
-                  </div> <br />
-                  <div>
-                    <p><button className="mini ui red button" onClick={this.props.deleteTask.bind(null, task.id)}>Del</button>
-                      <button className="mini ui button" onClick={this.props.displayEditModal.bind(null, task)}>Edit</button></p>
+                    <p>
+                      <button
+                        className="mini ui red button"
+                        onClick={this.props.deleteTask.bind(null, task.id)}
+                      >
+                        Del
+                      </button>
+                      <button
+                        className="mini ui button"
+                        onClick={this.props.displayEditModal.bind(null, task)}
+                      >
+                        Edit
+                      </button>
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        )
+        );
       }
-    })
-    const displayHighPriorityTasks = this.props.taskList.map((task) => {
-      taskCount++
-      if (task.priority === "high") {
+    });
+    const displayHighPriorityTasks = this.props.taskList.map(task => {
+      taskCount++;
+      if (
+        task.priority === "high" &&
+        this.props.currentUser.user_id === task.user_id
+      ) {
         return (
-          <div className="ui inverted segment" key={taskCount} style={this.setPriorityStyle(task)}>
+          <div
+            className="ui inverted segment"
+            key={taskCount}
+            style={this.setPriorityStyle(task)}
+          >
             <div role="list" className="ui divided inverted relaxed list">
               <div role="listitem" className="item">
                 <div className="content">
                   <div className="header">{task.taskname}</div>
                   Priority: {task.priority}
+                  <div>Description: {task.body}</div> <br />
                   <div>
-                    Description: {task.body}
-                  </div> <br />
-                  <div>
-                    <p><button className="mini ui red button" onClick={this.props.deleteTask.bind(null, task.id)}>Del</button>
-                      <button className="mini ui button" onClick={this.props.displayEditModal.bind(null, task)}>Edit</button></p>
+                    <p>
+                      <button
+                        className="mini ui red button"
+                        onClick={this.props.deleteTask.bind(null, task.id)}
+                      >
+                        Del
+                      </button>
+                      <button
+                        className="mini ui button"
+                        onClick={this.props.displayEditModal.bind(null, task)}
+                      >
+                        Edit
+                      </button>
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        )
+        );
       }
-    })
+    });
 
     return (
-
       <div className="TaskList-wrapper">
-        <div className="TaskList-col">
-          {displayHighPriorityTasks}
-        </div>
-        <div className="TaskList-col">
-          {displayMediumPriorityTasks}
-        </div>
-        <div className="TaskList-col">
-          {displayLowPriorityTasks}
-        </div>
-
+        <div className="TaskList-col">{displayHighPriorityTasks}</div>
+        <div className="TaskList-col">{displayMediumPriorityTasks}</div>
+        <div className="TaskList-col">{displayLowPriorityTasks}</div>
       </div>
-    )
+    );
   }
 }
 export default TaskList;
-
-
-
-
-
